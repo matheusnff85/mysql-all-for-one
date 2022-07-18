18a2583e4f813a08b51fefb4b26dcf93f62f280b
Matheus Marinho <math.marinho154@gmail.com>
2022-07-18 19:52:01 +0000

    feat: desafios de filtragem do numero 9 ao 13

---
+++ desafio12.sql
@@ -0,0 +1,2 @@
+SELECT submitted_date FROM northwind_dev.purchase_orders
+WHERE DATE(submitted_date) = '2006-04-26'; 
---
+++ desafio13.sql
@@ -0,0 +1,2 @@
+SELECT supplier_id FROM northwind_dev.purchase_orders
+WHERE supplier_id IN(1, 3);
