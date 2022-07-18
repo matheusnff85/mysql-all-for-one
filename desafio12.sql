0956ca8684489e608a1eb5636c63262aa9781a28
Matheus Marinho <math.marinho154@gmail.com>
2022-07-18 20:12:35 +0000

    fix: arrumando nome da database nos desafios 12 e 13

--- desafio12.sql
+++ desafio12.sql
@@ -1,2 +1,2 @@
-SELECT submitted_date FROM northwind_dev.purchase_orders
+SELECT submitted_date FROM northwind.purchase_orders
 WHERE DATE(submitted_date) = '2006-04-26'; 
--- desafio13.sql
+++ desafio13.sql
@@ -1,2 +1,2 @@
-SELECT supplier_id FROM northwind_dev.purchase_orders
+SELECT supplier_id FROM northwind.purchase_orders
 WHERE supplier_id IN(1, 3);
